d15aa43bca8d2b68864bfb109355abcbe5688a77
Capulus-Canis <143612912+Capulus-Canis@users.noreply.github.com>
2024-07-16 21:18:00 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -4,8 +4,17 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.get('/', (request, response) => {
     
-
- response.render("index")
+result = ""
+ response.render("formula",{result})
 })
-app.listen(11037)
+ app.post('/resultado',(request,response) =>{
+    let x = parseFloat(request.body.x)
+    let y = parseFloat(request.body.y)
+    let result = x/y
+    
+    response.render('formula',{result})
+    console.log(`${result}`)
+
+     
+app.listen(8080)
 console.log('Servidor rodando em https://http://localhost:8080')
