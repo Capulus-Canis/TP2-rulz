3421a06f085f9e28ec1eb94cae85db015e403cbd
Capulus-Canis <143612912+Capulus-Canis@users.noreply.github.com>
2024-07-23 23:21:07 -0300

    Update servidor.js

--- Aula 17-07/servidor.js
+++ Aula 17-07/servidor.js
@@ -2,14 +2,30 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-app.get('/', (request, response) => {
     
 result = ""
- response.render("rayane",{result})
+app.get('/conta', (request, response) => {
+   response.render ('rayane')
 })
- app.post('/resultado',(request,response) =>{
-    response.render('rayane',{result})
-    console.log(`${result}`)
- })
-     
-app.listen(8081)
+
+app.get('/soma', (request, response) => {
+   soma = parseFloat(request.query.v1) + parseFloat(request.query.v2)
+   response.render('rayane', {somaresult: soma})
+})
+
+app.get('/sub', (request, response) => {
+   sub = parseFloat(request.query.v1) - parseFloat(request.query.v2)
+   response.render('rayane', {subresult: sub})
+})
+
+app.get('/mult', (request, response) => {
+   mult = parseFloat(request.query.v1) * parseFloat(request.query.v2)
+   response.render('rayane', {multresult: mult}) 
+})
+
+app.get('/div', (request, response) => {
+   div = parseFloat(request.query.v1) / parseFloat(request.query.v2)
+   response.render('rayane', {divresult: div})
+})
+app.listen(8080)
+    console.log('Servidor rodando em https://http://localhost:8080')
